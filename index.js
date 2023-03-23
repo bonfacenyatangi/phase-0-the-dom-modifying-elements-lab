a980cab00fbc062d3dd75e80507580896b53913c
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-03-23 11:40:12 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+let deleteElement = document.getElementById('main');
+
+deleteElement.remove();
+
+// Creating a new Header element and assigning it an id of 'victory'
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+// Adding content into my header
+let myName = "Bonface";
+newHeader.textContent = `${myName} is the champion`;
+
+//Appending content into my new header
+newHeader.appendChild(newHeader);
